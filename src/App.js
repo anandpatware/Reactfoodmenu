cd563f3fead0baa767e0b375ff861560aef59e70
anandpat <anandpatware123@gmail.com>
2023-10-23 02:22:20 +0530

    some more data added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import Card from "./Components/Card";
 import { Route, Routes } from "react-router-dom";
 import foodData from "./Components/fooddata";
+import { dinner, lunch } from "./Components/fooddata";
 
 function App() {
   return (
@@ -34,7 +35,53 @@ function App() {
             </div>
           }
         />
-        <Route path="/lunch" element={<Card />} />
+        <Route
+          path="/lunch"
+          element={
+            <div
+              className="card-container"
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+              }}
+            >
+              {lunch.map((ele, index) => (
+                <Card
+                  name={ele.name}
+                  image={ele.imageUrl}
+                  desc={ele.description}
+                  price={ele.price}
+                  key={index}
+                />
+              ))}
+            </div>
+          }
+        />
+
+        <Route
+          path="/dinner"
+          element={
+            <div
+              className="card-container"
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+              }}
+            >
+              {dinner.map((ele, index) => (
+                <Card
+                  name={ele.name}
+                  image={ele.imageUrl}
+                  desc={ele.description}
+                  price={ele.price}
+                  key={index}
+                />
+              ))}
+            </div>
+          }
+        ></Route>
       </Routes>
     </div>
   );
--- src/Components/fooddata.js
+++ src/Components/fooddata.js
@@ -139,4 +139,105 @@ const foodData = [
   },
 ];
 
+const dinner = [
+  {
+    imageUrl:
+      "https://www.thegrilledcheesetruck.com/wp-content/uploads/2017/03/gct-menu21-800x600.jpg",
+    name: "Burger",
+    description: "Delicious burger with all the fixings",
+    price: 5.99,
+  },
+  {
+    imageUrl:
+      "https://blueprint-api-production.s3.amazonaws.com/uploads/card/image/917437/3261d336-f53b-44a0-abdb-69792f60af66.jpg",
+    name: "Pizza",
+    description: "A classic pizza with your favorite toppings",
+    price: 9.99,
+  },
+  {
+    imageUrl:
+      "https://tastychomps.com/wp-content/uploads/2019/06/P6150451-800x600.jpg",
+    name: "Sushi",
+    description: "Fresh sushi rolls made to order",
+    price: 12.99,
+  },
+  {
+    imageUrl:
+      "https://cdn.styleblueprint.com/wp-content/uploads/2019/07/SB-BHM-Food-Truck-Mustard-Seed-Food-Co-hot-mess-fries.jpg",
+    name: "Pasta",
+    description: "Homemade pasta with rich tomato sauce",
+    price: 8.99,
+  },
+];
+const lunch = [
+  {
+    imageUrl:
+      "https://porteliotfestival.com/wp-content/uploads/2017/04/Port-Eliot-Festival-2017-294A3665-800x600.jpg",
+    name: "Sandwich",
+    description: "Freshly made sandwiches with your choice of fillings",
+    price: 6.49,
+  },
+  {
+    imageUrl:
+      "https://www.mykadhai.com/assets/images/img-4142-2000x1500-800x600.jpg",
+    name: "Soup",
+    description: "Hot and comforting soups for all seasons",
+    price: 4.99,
+  },
+  {
+    imageUrl:
+      "https://nwatravelguide.com/wp-content/uploads/2018/05/The-Hive-800x600.jpg",
+    name: "BBQ Ribs",
+    description: "Tender BBQ ribs with smoky flavor",
+    price: 13.99,
+  },
+  {
+    imageUrl: "https://www.mjseatery.com/images/buritto.jpg",
+    name: "Fish & Chips",
+    description: "Classic fish and chips with tartar sauce",
+    price: 9.49,
+  },
+  {
+    imageUrl:
+      "https://imagevars.gulfnews.com/2019/07/18/Deccan-Delight-biryani_16c043a76bd_original-ratio.jpg",
+    name: "Vegetarian Curry",
+    description: "Spicy vegetarian curry with rice",
+    price: 11.49,
+  },
+  {
+    imageUrl:
+      "https://www.mykadhai.com/assets/images/img-4142-2000x1500-800x600.jpg",
+    name: "Cheesecake",
+    description: "Rich and creamy cheesecake with toppings",
+    price: 5.99,
+  },
+  {
+    imageUrl:
+      "https://imagevars.gulfnews.com/2019/07/18/Deccan-Delight-biryani_16c043a76bd_original-ratio.jpg",
+    name: "Milkshake",
+    description: "Thick and indulgent milkshakes in various flavors",
+    price: 4.99,
+  },
+  {
+    imageUrl:
+      "https://imagevars.gulfnews.com/2019/07/18/Deccan-Delight-biryani_16c043a76bd_original-ratio.jpg",
+    name: "Donuts",
+    description: "Freshly baked donuts with glaze or sprinkles",
+    price: 2.49,
+  },
+  {
+    imageUrl: "https://www.mjseatery.com/images/buritto.jpg",
+    name: "Fruit Salad",
+    description: "Refreshing fruit salad with seasonal fruits",
+    price: 3.99,
+  },
+  {
+    imageUrl:
+      "https://imagevars.gulfnews.com/2019/07/18/Deccan-Delight-biryani_16c043a76bd_original-ratio.jpg",
+    name: "Cupcakes",
+    description: "Assorted cupcakes with colorful frosting",
+    price: 2.99,
+  },
+];
 export default foodData;
+export { dinner, lunch };
